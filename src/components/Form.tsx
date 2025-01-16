a719f9b1b8f8ed9bb9f4d64de5e1e473a8b1950f
D3vEduardo <eduardoaugustolimabueno@outlook.com.br>
2025-01-16 17:10:15 -0300

    Adicionando animações ao form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,7 @@ export default function Form({ children }: iProps) {
     return (
         <form className="flex flex-col gap-6 bg-zinc-100
         border-2 px-6 py-8 rounded-lg items-center justify-center
-        max-w-[90%]">
+        max-w-[90%] hover:scale-105 transition-all duration-300">
             {children}
         </form>
     )
